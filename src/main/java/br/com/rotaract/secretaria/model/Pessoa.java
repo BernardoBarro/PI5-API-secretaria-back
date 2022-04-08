f331d7e53d6f70124a66030cca4da5d052d494a2
Bernardo Barro <bernardo.barro@gmail.com>
2022-04-08 20:14:42 -0300

    feat start project

---
+++ src/main/java/br/com/rotaract/secretaria/model/Pessoa.java
@@ -0,0 +1,42 @@
+package br.com.rotaract.secretaria.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Pessoa {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String nome;
+	
+	private int idade;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+}
---
+++ src/test/java/br/com/rotaract/Secretaria/SecretariaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rotaract.Secretaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecretariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
