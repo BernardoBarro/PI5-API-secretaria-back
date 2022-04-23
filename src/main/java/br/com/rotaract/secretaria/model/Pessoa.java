c557b2099f2820462c5e71134d68fb16b6bfb06f
BernardoBarro <bernardo.barro@gmail.com>
2022-04-23 00:25:28 -0300

    Revert "pls work"
    This reverts commit bad78e4924988133160794d0d68d47ea7bff2c50.

--- src/main/java/br/com/rotaract/secretaria/model/Pessoa.java
+++ src/main/java/br/com/rotaract/secretaria/model/Pessoa.java
@@ -1,7 +1,15 @@
 package br.com.rotaract.secretaria.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Pessoa {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
 	private String nome;
