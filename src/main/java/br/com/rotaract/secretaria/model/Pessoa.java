bad78e4924988133160794d0d68d47ea7bff2c50
BernardoBarro <bernardo.barro@gmail.com>
2022-04-23 00:23:03 -0300

    pls work

--- src/main/java/br/com/rotaract/secretaria/model/Pessoa.java
+++ src/main/java/br/com/rotaract/secretaria/model/Pessoa.java
@@ -1,15 +1,7 @@
 package br.com.rotaract.secretaria.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-
-@Entity
 public class Pessoa {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
 	private String nome;
