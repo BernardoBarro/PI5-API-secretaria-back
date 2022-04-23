546cd4749c9099e08d7a0cf32e2cfd4e156552e3
BernardoBarro <bernardo.barro@gmail.com>
2022-04-22 21:35:41 -0300

    teste

--- src/main/java/br/com/rotaract/secretaria/controller/HelloController.java
+++ src/main/java/br/com/rotaract/secretaria/controller/HelloController.java
@@ -1,13 +1,13 @@
 package br.com.rotaract.secretaria.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
 	
-	@RequestMapping("/")
+	@GetMapping("/")
 	@ResponseBody
 	public String hello() {
 		return "Olá, Mundo!";
