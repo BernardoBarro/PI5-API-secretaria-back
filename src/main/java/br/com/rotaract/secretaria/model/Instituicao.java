65e02a0ab17276be68f10588a8b8b827e3b0a42f
Matheus Galvain <matheusgalvain@gmail.com>
2022-05-06 21:23:20 -0300

    Arrumando o banco

--- src/main/java/br/com/rotaract/secretaria/model/Instituicao.java
+++ src/main/java/br/com/rotaract/secretaria/model/Instituicao.java
@@ -12,7 +12,7 @@ import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
 @AllArgsConstructor
-@Table(name = "instituicoesbeneficiadas")
+@Table(name = "instituicoes_beneficiadas")
 @Entity
 public class Instituicao {
 
