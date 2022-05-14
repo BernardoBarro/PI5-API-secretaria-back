6c9cc3243adb49978411470cb56bb35ee458b795
Caroline Kolassa <carolinekolassa@gmail.com>
2022-05-13 23:20:05 -0300

    Crud_Patrocinador

--- src/main/java/br/com/rotaract/secretaria/controller/PatrocinadorController.java
+++ src/main/java/br/com/rotaract/secretaria/controller/PatrocinadorController.java
@@ -1,7 +1,12 @@
 package br.com.rotaract.secretaria.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,6 +26,26 @@ public class PatrocinadorController {
 	public Patrocinador createPatrocinador(@RequestBody PatrocinadorDto patrocinadorDto) {
 
 		return service.createPatrocinador(patrocinadorDto);
-
 	}
+	
+	@GetMapping
+	public List<Patrocinador> listPatrocinador() {
+		
+		return service.findPatrocinador();	
+	}
+	
+	@GetMapping("/{id}")
+	public Patrocinador getPatrocinador(@PathVariable Long id) {
+		
+		return service.findPatrocinador(id);
+	}
+	
+	@PutMapping("/{id}")
+	public Patrocinador editPatrocinador(@PathVariable Long id, 
+			@RequestBody PatrocinadorDto patrocinadorEditDto) {
+		
+		return service.updatePatrocinador(id, patrocinadorEditDto);
+		
+	}
+	
 }
