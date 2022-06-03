af09ca9680f2b94b5187ed2e9fd6406da60c3562
BernardoBarro <bernardo.barro@gmail.com>
2022-06-03 20:00:43 -0300

    fix cors configuration

--- src/main/java/br/com/rotaract/secretaria/config/CorsConfiguration.java
+++ src/main/java/br/com/rotaract/secretaria/config/CorsConfiguration.java
@@ -1,14 +1,20 @@
 package br.com.rotaract.secretaria.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class CorsConfiguration implements WebMvcConfigurer {
 
+	@Value("${cors.origin}")
+	private String origin;
+	
 	@Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("http://localhost:8080")
+            .allowedOrigins(origin)
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS",  "HEAD", "TRACE", "CONNECT");
     }
 }
