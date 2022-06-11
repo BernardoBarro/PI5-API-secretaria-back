bc539de7eb0009157bb0786f9c10959457e12896
BernardoBarro <bernardo.barro@gmail.com>
2022-06-11 11:59:04 -0300

    tentando

--- src/main/java/br/com/rotaract/secretaria/controller/AutenticacaoController.java
+++ src/main/java/br/com/rotaract/secretaria/controller/AutenticacaoController.java
@@ -37,7 +37,7 @@ public class AutenticacaoController {
 			String token = tokenService.gerarToken(authentication);
 			return ResponseEntity.ok(new TokenDto(token, "Bearer"));
 		} catch (AuthenticationException e) {
-			return ResponseEntity.badRequest().build();
+			throw e;
 		}
 		
 	}
